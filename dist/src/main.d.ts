48e22d3d1e28d24a3e149cff6beee8c62beb4e22
jesse <jessexinyu@foxmail.com>
2021-05-17 16:02:23 +0800

    update .gitignore

--- dist/src/main.d.ts
+++
@@ -1,7 +0,0 @@
-import * as ab from './core/ab';
-import eventBus from './core/eventBus';
-declare const _default: {
-    eventBus: typeof eventBus;
-    ab: typeof ab;
-};
-export default _default;
